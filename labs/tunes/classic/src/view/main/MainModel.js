e6ad714fd7837be9d5391bc8917addb0fc0ef26b
Dayananda D <dayananda.d@CELSYSWTC.IN>
2022-04-28 14:11:23 +0530

    added sencha training file

---
+++ labs/tunes/classic/src/view/main/MainModel.js
@@ -0,0 +1,12 @@
+Ext.define('Tunes.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.main-main',
+    requires: ['Tunes.model.Tune'],
+    stores: {
+        tunes: {
+            model: 'Tunes.model.Tune',
+            autoLoad: true,
+            sorters: ['artist', 'title']
+        }
+    }
+});
