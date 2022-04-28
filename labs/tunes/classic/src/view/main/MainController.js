1599b7abe15bb1c0bf489fe110cb7a725c86bdfc
Dayananda D <dayananda.d@CELSYSWTC.IN>
2022-04-28 19:55:04 +0530

    changes in daya

--- labs/tunes/classic/src/view/main/MainController.js
+++ labs/tunes/classic/src/view/main/MainController.js
@@ -1,10 +1,4 @@
 Ext.define('Tunes.view.main.MainController', {
-<<<<<<< HEAD
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.main-main'
-
-=======
     extend: 'Tunes.view.main.BaseController',
     alias: 'controller.main-main'
->>>>>>> main
 });
